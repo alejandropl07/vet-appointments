a4fd4da07e2f726dfc1979b66a279a8a9e286714
alejandropl07 <aprietoleon97@gmail.com>
2022-05-18 09:52:01 -0500

    State

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React, { useState, Fragment } from "react";
 import Form from "./components/Form";
 
 function App() {
-  const [quotes, saveQuotes] = useState([]);
+  const [appointments, saveAppointment] = useState([]);
+
+  const createAppointment = (appointment) => {
+    const newsAppointments = [...appointments, appointment];
+    saveAppointment(newsAppointments);
+  };
 
   return (
     <Fragment>
@@ -10,7 +15,7 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="one-half  column">
-            <Form/>
+            <Form createAppointment={createAppointment} />
           </div>
           <div className="one-half  column"></div>
         </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,39 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
-function Form() {
+function Form({ createAppointment }) {
+  const [appointment, updateAppointment] = useState({
+    mascot: "",
+    owner: "",
+    date: "",
+    time: "",
+    symptom: "",
+  });
+
+  const updateState = (e) => {
+    updateAppointment({
+      ...appointment,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const sendAppointment = (e) => {
+    e.preventDefault();
+    createAppointment(appointment);
+    console.log(appointment);
+  };
+
   return (
     <Fragment>
       <h2>Create appointment</h2>
 
-      <form>
+      <form onSubmit={sendAppointment}>
         <label>Mascot name</label>
         <input
           type="text"
           name="mascot"
           className="u-full-width"
           placeholder="Mascot name"
+          onChange={updateState}
         />
 
         <label>Owner name</label>
@@ -20,16 +42,31 @@ function Form() {
           name="owner"
           className="u-full-width"
           placeholder="Owner name"
+          onChange={updateState}
         />
 
         <label>Date</label>
-        <input type="date" className="u-full-width" name="date" />
+        <input
+          type="date"
+          className="u-full-width"
+          name="date"
+          onChange={updateState}
+        />
 
         <label>Time</label>
-        <input type="time" className="u-full-width" name="time" />
+        <input
+          type="time"
+          className="u-full-width"
+          name="time"
+          onChange={updateState}
+        />
 
         <label>Symptom</label>
-        <textarea className="u-full-width" name="symptom"></textarea>
+        <textarea
+          className="u-full-width"
+          name="symptom"
+          onChange={updateState}
+        ></textarea>
 
         <button type="submit" className="button-primary u-full-width">
           Add
